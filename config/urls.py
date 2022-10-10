1fb41ddc24ad9f5b7a479e6c75dbab46a93ff8dc
portion-jack <flycast0080@gmail.com>
2022-10-10 20:24:21 +0900

    2nd commit

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from pybo import views
 
 # from pybo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
-    path('common/',include('common.urls')),
-    path('',views.index,name='index'),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),
 ]
